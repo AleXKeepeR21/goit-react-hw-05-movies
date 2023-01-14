db71264807790f05015feddeaca666d3df743e95
AleXKeepeR21 <106617654+AleXKeepeR21@users.noreply.github.com>
2023-01-14 20:53:11 +0200

    add fetch

---
+++ src/components/FetchMovies/FetchMovies.jsx
@@ -0,0 +1,67 @@
+const apiUrl = 'https://api.themoviedb.org/3';
+const apiKey = '18cb12699e14d8de9e9367a7c1c3c558';
+
+export function fetchGetTrending(page = 1) {
+  return fetch(`${apiUrl}/trending/movie/day?api_key=${apiKey}&page=${page}`);
+}
+
+export function fetchSearchMovies(query, page = 1) {
+  return fetch(`${apiUrl}/search/movie?api_key=${apiKey}&query=${query}&language=en-US&page=${page}&include_adult=false
+    `);
+}
+
+export function fetchGetMovieDetails(id) {
+  return fetch(`${apiUrl}/movie/${id}?api_key=${apiKey}&language=en-US
+`);
+}
+
+export function FetchGetMovieCredits(id) {
+  return fetch(`${apiUrl}/movie/${id}/credits?api_key=${apiKey}&language=en-US
+  `);
+}
+
+export function FetchGetMovieReviews(id, page = 1) {
+  return fetch(`${apiUrl}/movie/${id}/reviews?api_key=${apiKey}&language=en-US&page=${page}
+  `);
+}
+
+// import axios from 'axios';
+
+// axios.defaults.baseURL = `https://api.themoviedb.org/3/`;
+// const KEY = '18cb12699e14d8de9e9367a7c1c3c558';
+
+// export const fetchGetTrending = async page => {
+//   const response = await axios.get(
+//     `trending/movie/day?api_key=${KEY}&page=${page}`
+//   );
+
+//   return response.data;
+// };
+
+// export const fetchSearchMovies = async (query, page) => {
+//   const response = await axios.get(
+//     `search/movie?api_key=${KEY}&query=${query}&language=en-US&page=${page}&include_adult=false`
+//   );
+
+//   return response.data;
+// };
+
+// export const fetchGetMovieDetails = async id => {
+//   const response = await axios.get(`movie/${id}?api_key=${KEY}&language=en-US`);
+
+//   return response.data;
+// };
+
+// export const FetchGetMovieCredits = async id => {
+//   const response = await axios.get(
+//     `movie/${id}/credits?api_key=${KEY}&language=en-US`
+//   );
+//   return response.data.cast;
+// };
+
+// export const FetchGetMovieReviews = async id => {
+//   const response = await axios.get(
+//     `movie/${id}/reviews?api_key=${KEY}&language=en-US&page=1`
+//   );
+//   return response.data.results;
+// };
